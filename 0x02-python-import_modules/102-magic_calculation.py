7bd56cd52c073199561f32706b0ab6b12a875c1d
KevCasHSC <castrok.c11@hotmail.com>
2019-12-13 04:30:18 +0000

    last upload

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -6,5 +6,7 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             c = add(c, i)
             return c
-        else:
+    else:
             return sub(a, b)
+from dis import dis
+dis(magic_calculation)
