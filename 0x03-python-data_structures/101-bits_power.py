2c30b36b633cb321ec65a13bc62f35d5297e9175
KevCasHSC <castrok.c11@hotmail.com>
2020-01-07 15:10:06 +0000

    last upload

---
+++ 0x03-python-data_structures/101-bits_power.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def bits_power(n=0):
+    k = []
+    for c in my_list:
+        if c ** 2 == 0:
+            k.append(True)
+        else:
+            k.append(False)
+    return k
