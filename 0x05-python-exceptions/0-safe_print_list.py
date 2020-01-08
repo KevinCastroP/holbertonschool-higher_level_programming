5588a0fef34fd168d55df6a4258d265003f37d43
KevCasHSC <castrok.c11@hotmail.com>
2020-01-08 19:07:34 +0000

    last upload

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    try:
+        for k in range(x):
+            print("{}".format(my_list[k]), end="")
+            count += 1
+        print()
+        return count
+    except IndexError:
+        print()
+        return k
