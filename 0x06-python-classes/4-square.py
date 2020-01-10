a8f348728bf1664e730f1cc6630cfd417fb72eb1
KevCasHSC <castrok.c11@hotmail.com>
2020-01-10 03:09:28 +0000

    last upload

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+class Square:
+    """Access and update private attribute"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else: TypeError("size must be an integer")
+
+    def area(self):
+        """# of the area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """print the square size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter the square area"""
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
