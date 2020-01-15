9bf9f9f40a53e3b81d78f0295d66048891e5dd5a
KevCasHSC <castrok.c11@hotmail.com>
2020-01-15 19:06:40 +0000

    last upload

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+class to define a rectangle. Deleting rectangle
+"""
+
+
+class Rectangle:
+    """class rectangle"""
+
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances = Rectangle.number_of_instances + 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, a):
+        if not isinstance(a, int):
+            raise TypeError("width must be an integer")
+        if a < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = a
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, b):
+        if not isinstance(b, int):
+            raise TypeError("height must be an integer")
+        if b < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = b
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        steward = ""
+        if self.__width == 0 or self.__height == 0:
+            return steward
+        return "\n".join(["#" * self.width] * self.height)
+
+    def __repr__(self):
+        command = "Rectangle(" + str(self.__width)
+        command = command + ", " + str(self.__height)
+        command = command + ")"
+        return command
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances = Rectangle.number_of_instances - 1
