0a26aa71af625f47a2a3935d7f6b4c1cfce6c79b
KevCasHSC <castrok.c11@hotmail.com>
2020-01-20 22:18:12 +0000

    last upload

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Same class or inherit from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """function to verify an object type inherit"""
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
