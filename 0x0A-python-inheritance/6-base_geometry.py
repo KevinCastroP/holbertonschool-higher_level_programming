716d49b6656b5446c1f976b644df44fb7478d668
KevCasHSC <castrok.c11@hotmail.com>
2020-01-21 01:25:32 +0000

    last upload

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Improve Geometry
+class with public instance area
+"""
+
+
+class BaseGeometry:
+    """base geo class"""
+    def area(self):
+        """area function raises area not implemeted"""
+        raise Exception("area() is not implemented")
