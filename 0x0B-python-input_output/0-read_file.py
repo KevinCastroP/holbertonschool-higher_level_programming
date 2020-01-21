cfd5fe48a1fdffd97e8bd6023c274283c7e2f96c
KevCasHSC <castrok.c11@hotmail.com>
2020-01-21 19:40:00 +0000

    last upload

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Read ile
+function to read a text file
+and printed
+"""
+
+
+def read_file(filename=""):
+    """method to read a file text"""
+    with open('my_file_0.txt', 'r') as k:
+        for text in k:
+            print (text, end="")
