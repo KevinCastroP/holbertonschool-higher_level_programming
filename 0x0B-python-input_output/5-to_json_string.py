02a0afe3d8a9c48b470156d9b643e550fc737e25
KevCasHSC <castrok.c11@hotmail.com>
2020-01-22 03:04:32 +0000

    last upload

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+JSON string
+function to return
+the json representation of an obj
+"""
+
+
+def to_json_string(my_obj):
+    """JSON string"""
+    return json.dumps(my_obj)
