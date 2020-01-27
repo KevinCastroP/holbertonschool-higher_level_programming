5df131265dffbf3405d52aaec568d07540199b20
KevCasHSC <castrok.c11@hotmail.com>
2020-01-27 22:58:31 +0000

    last upload

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,3 +12,11 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """creator"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """string representation of square"""
+        my_id = self.id
+        x = super().x
+        y = super().y
+        w = super().width
+        return "[Square] ({}) {}/{} - {}".format(my_id, x, y, w)
