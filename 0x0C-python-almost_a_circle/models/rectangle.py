cf3d02b9c9a983f0ad8ff6ac37e50e499844677e
KevCasHSC <castrok.c11@hotmail.com>
2020-01-27 19:03:20 +0000

    last upload

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,7 @@ class Rectangle(Base):
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
+
+    def area(self):
+        """adding area to rectangle"""
+        return self.__width * self.__height
