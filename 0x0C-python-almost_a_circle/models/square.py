47872f459b494ebbbf110b0ae72b425a30d80355
KevCasHSC <castrok.c11@hotmail.com>
2020-01-27 22:44:48 +0000

    last upload

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+creating a class square
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """And now, the Square!"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """creator"""
+        super().__init__(size, size, x, y, id)
+        self.size = size
