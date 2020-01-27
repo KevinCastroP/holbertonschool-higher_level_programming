09fc758c5798505c217fcb79f3a8e00ead0a9531
KevCasHSC <castrok.c11@hotmail.com>
2020-01-27 20:43:26 +0000

    last upload

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -88,5 +88,4 @@ class Rectangle(Base):
     def __str__(self):
         """string representation of rectangle"""
         return "[{}] ({}) {}/{} - {}/{}"\
-        .format(type(self).__name__, self.id, self.__x, self.__y, self.__width,
-self.__height)
+            .format(type(self).__name__, self.id, self.__x, self.__y, self.__width, self.__height)
