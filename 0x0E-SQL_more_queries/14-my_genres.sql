48d0251afa9c1f99ff6dd4d82c0ec0336671f928
KevCasHSC <castrok.c11@hotmail.com>
2020-03-03 23:44:10 +0000

    last upload

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,8 @@
+-- Use a table of DB
+-- And list all geners
+SELECT tv_genres.name
+FROM tv_show_genres
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_shows.title = 'Dexter'
+ORDER BY tv_genres.name;
