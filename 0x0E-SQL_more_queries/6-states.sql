5cba37fce54486f1e6927426199da2b6cef448c7
KevCasHSC <castrok.c11@hotmail.com>
2020-03-03 21:49:01 +0000

    last upload

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- Create a DB hbtn_0d_usa
+-- With the table states description id and name
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
