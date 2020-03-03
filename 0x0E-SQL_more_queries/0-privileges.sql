8de5a20c78c3d2be856e0e09065f141aa1e32b52
KevCasHSC <castrok.c11@hotmail.com>
2020-03-03 20:33:40 +0000

    last upload

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
--- List all privileges of user in mysql
+-- Lists all privileges of the MySQL users user_0d_1 and user_0d_2
 -- Show the privileges of many users
-SHOW GRANTS FOR user_0d_1;
-SHOW GRANTS FOR user_0d_2;
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
