65eccfbb4ec2405d4d0d414ced8a8c733fcc44a3
KevCasHSC <castrok.c11@hotmail.com>
2020-03-03 22:05:56 +0000

    last upload

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,4 +2,4 @@
 -- And the table cities with id, state_id, name
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, CONSTRAIN 'fk01_state_cities' FOREIGN KEY (state_id) REFERENCES state(id));
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES state(id));
