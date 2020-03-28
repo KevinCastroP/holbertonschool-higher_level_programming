095ce41de7fce1399249ec51c14a3969e1906b55
KevCasHSC <castrok.c11@hotmail.com>
2020-03-28 20:20:16 +0000

    last upload

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                          charset="utf8")
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '%{}%'".format(argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '%{}%'".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
