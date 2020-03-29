f0ca14fb1b8ee5921311c7ea3a2479205181677b
KevCasHSC <castrok.c11@hotmail.com>
2020-03-29 13:30:52 +0000

    last upload

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Listing all objects from State table
+"""
+
+
+if __name__ == '__main__':
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.contains("a")).all()
+    for state in states:
+        session.delete(state)
+    session.commit()
+    session.close()
