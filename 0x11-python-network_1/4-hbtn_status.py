77210c5a05f9ac76498519ca8fec6dacaeb0e78a
KevCasHSC <castrok.c11@hotmail.com>
2020-04-15 17:09:48 +0000

    last upload

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     import requests
 
     r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("    - type: {}".format(type(r.text)))
-    print("    - content: {}".format(r.text))
+    body = r.text
+    print('Body response:')
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
