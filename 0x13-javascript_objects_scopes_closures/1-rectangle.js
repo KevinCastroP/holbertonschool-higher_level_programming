c28ad32c70acb359727545f8e1d01c4a3dc1216d
KevinCastroP <castrok.c11@hotmail.com>
2020-05-11 14:43:30 -0500

    task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+Creating an empty class Rectangle
+*/
+
+class Rectangle {
+    constructor (w, h) {
+        this.width = w;
+        this.height = h;
+    }
+}
+
+module.exports = Rectangle;
