f954a82d52886583eb6f9f7e830f4d2dfb2b256f
KevinCastroP <castrok.c11@hotmail.com>
2020-05-11 15:23:59 -0500

    fixing spaces

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -4,10 +4,10 @@ Creating an empty class Rectangle
 */
 
 class Rectangle {
-    constructor (w, h) {
-        this.width = w;
-        this.height = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
 
 module.exports = Rectangle;
