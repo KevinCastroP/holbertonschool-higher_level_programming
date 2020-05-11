7256f8f66e09e190392760fec61fa996e7cab4aa
KevinCastroP <castrok.c11@hotmail.com>
2020-05-11 18:52:27 -0500

    fixing errors, changing var to const and let

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,10 +4,10 @@ Creating an empty class Rectangle
 */
 
 exports.esrever = function (list) {
-  var arrayOne = list;
-  var array2 = [];
+  const arrayOne = list;
+  const array2 = [];
 
-  for (var i = arrayOne.length - 1; i >= 0; i--) {
+  for (let i = arrayOne.length - 1; i >= 0; i--) {
     array2.push(arrayOne[i]);
   }
   return array2;
