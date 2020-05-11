9f27723ba7311306deec467bddf227320ea39699
KevinCastroP <castrok.c11@hotmail.com>
2020-05-11 17:46:58 -0500

    task 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+Creating an empty class Rectangle
+*/
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
