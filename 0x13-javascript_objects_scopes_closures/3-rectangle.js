3d77d49e8c10610bbdc204bf1308ec811c49edef
KevinCastroP <castrok.c11@hotmail.com>
2020-05-11 15:52:36 -0500

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+/*
+Creating an empty class Rectangle
+*/
+
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && Number.isInteger(h) &&
+    w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let height = 0; height < this.width; height++) {
+      console.log('#'.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Rectangle;
