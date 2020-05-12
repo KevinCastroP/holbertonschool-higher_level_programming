ddfef7da8e86937c90837241675a2593585ec7b5
KevinCastroP <castrok.c11@hotmail.com>
2020-05-12 18:52:18 -0500

    last update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,7 @@
 // Script that reads and prints the content of a file.
 const fs = require('fs');
 const file = process.argv[2];
-fs.readFile(file, 'utf8', function read(err, data) {
+fs.readFile(file, 'utf8', function read (err, data) {
   if (err) {
     console.log(err);
   } else {
