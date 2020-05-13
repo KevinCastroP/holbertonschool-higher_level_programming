739193ed2c7a66bf695048da587d24519a1cb97f
KevinCastroP <castrok.c11@hotmail.com>
2020-05-12 19:02:11 -0500

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,10 +4,8 @@ const fs = require('fs');
 const file = process.argv[2];
 const stringg = process.argv[3];
 
-fs.writeFile(file, stringg, 'utf8', function read (err, data) {
+fs.writeFile(file, stringg, 'utf8', function write (err) {
   if (err) {
     console.log(err);
-  } else {
-    console.log(data);
-  }
+  } 
 });
