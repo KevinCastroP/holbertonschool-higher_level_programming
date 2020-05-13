7774a2e9d9242a291e0d910f8b7b5dad100de3b6
KevinCastroP <castrok.c11@hotmail.com>
2020-05-12 19:51:15 -0500

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // Script that reads and prints the content of a file.
 const request = require('request');
+const url = process.argv[2];
 
-request(process.argv[2], (error, response, body) => {
-  if (error) {
-    console.log(error);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log('code:', response.statusCode);
   } else {
-    console.log('code:', response && response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
